68d2bbe348f31344ed68ac0555d9f188243c5e39
omarmaamoun179 <omarmaamoun6@gmail.com>
2023-09-21 20:05:40 +0300

    create appTheme file

---
+++ lib/core/app_theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme{
+  ThemeData appTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.transparent,
+    primaryColor: const Color(0xff39A552),
+  );
+}
