f13c1b915d47be16a975b8d78ac4deb4e3ff02b6
omarmaamoun179 <omarmaamoun6@gmail.com>
2023-09-26 18:46:43 +0300

    create file has constnat of api constant and create function to get sources

---
+++ lib/constant/constant.dart
@@ -0,0 +1,7 @@
+class Constant {
+  static const String apiKey = 'ef38ab91af1043d9aec12bc10721eda9';
+  static const String baseUrl = '//newsapi.org';
+  static const String topHeadlines = '/v2/top-headlines';
+  static const String everything = '/v2/everything';
+  static const String sources = '/v2/sources';
+}
--- lib/core/network/api_manager.dart
+++ lib/core/network/api_manager.dart
@@ -1,1 +1,16 @@
-import 'package:http/browser_client.dart' as http;
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:news_app_route/constant/constant.dart';
+
+class ApiManger {
+  Future<http.Response> getSources() async {
+    Uri url = Uri.https(Constant.baseUrl, Constant.sources, {
+      'apiKey': Constant.apiKey,
+    });
+    var response = await http.get(url);
+    jsonDecode(response.body);
+    print(response.body);
+    return response;
+  }
+}
